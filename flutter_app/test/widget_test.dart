5a26c6b615bfebb9ccdf3a4c1665c3e41a2765cd
developer-fox <muhammetbozkurt974@gmail.com>
2022-09-27 01:17:53 +0300

    flutter_app architecture was pulled

--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/main.dart';
+import 'package:company_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
